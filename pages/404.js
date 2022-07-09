413fea3a6ca1f4724157b79572b3d97520a1041e
abduh <muhammad.abdurahman@ui.ac.id>
2022-07-09 23:42:42 +0700

    adjust zindex for 404 page

--- pages/404.js
+++ pages/404.js
@@ -5,8 +5,8 @@ import Layout from '../components/Layout'
 export default function FourOhFour() {
     return <Layout hasNavbar hasFooter>
 
-        <Flex justify="center" flexDirection="column" align="center" w="100%" minH="100vh">
-            <Image opacity={0.3} left="-20%" top="-5%" position="absolute" w="1300px" h="1400px" src="/bg-partners.png" alt="wavy bg" />
+        <Flex position="relative" justify="center" flexDirection="column" align="center" w="100%" minH="100vh">
+            <Image opacity={0.3} zIndex="0" left="-40%" top="-5%" position="absolute" w="1300px" h="1400px" src="/bg-partners.png" alt="wavy bg" />
             <Text
                 fontWeight={800}
                 fontSize="150px"
@@ -14,16 +14,19 @@ export default function FourOhFour() {
                 className="gradientText primaryFont">
                 Oops!
             </Text>
-            <Link href="/">
-                <a>
-                    <Text className="primaryFont" fontSize="32px" lineHeight={1.2} fontWeight={800} textAlign="center">
-                        {"This page is still under construction."}
-                    </Text>
-                    <Text className="primaryFont" fontSize="32px" lineHeight={1.2} fontWeight={800} textAlign="center">
-                        {"Why don't we explore other pages?"}
-                    </Text>
-                </a>
-            </Link>
+            <Box zIndex="1" mt="24px">
+                <Link href="/">
+                    <a>
+                        <Text className="primaryFont" fontSize="32px" lineHeight={1.2} fontWeight={800} textAlign="center">
+                            {"This page is still under construction."}
+                        </Text>
+                        <Text className="primaryFont" fontSize="32px" lineHeight={1.2} fontWeight={800} textAlign="center">
+                            {"Why don't we explore other pages?"}
+                        </Text>
+                    </a>
+                </Link>
+            </Box>
+
         </Flex>
 
     </Layout>
