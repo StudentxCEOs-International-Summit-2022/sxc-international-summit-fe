2ea0bea5cc5f5bf8b10958fb88406b2592094afe
abduh <muhammad.abdurahman@ui.ac.id>
2022-07-11 15:53:22 +0700

    add nextseo

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { NextSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <NextSeo
+        title="StudentsxCEOs International Summit 2022"
+        description="A Grand Event organized by StudentsxCEOs Jakarta consisting of a sequence of enriching and groundbreaking events."
+        
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   )
